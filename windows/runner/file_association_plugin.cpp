5d693116263538c5253f27a2dd5709bdb65b7b56
weimi95 <91066116+weimi95@users.noreply.github.com>
2026-09-12 17:18:18 +0800

    fix(windows): MethodChannel 名对齐 Dart 侧，修复文件关联注册失效

--- windows/runner/file_association_plugin.cpp
+++ windows/runner/file_association_plugin.cpp
@@ -25,7 +25,7 @@ void FileAssociationPluginRegisterWithRegistrar(
 
   channel_ =
       std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "com.weimi95.weimi_file/file_association",
+          registrar->messenger(), "com.weimi95.weimi/file_association",
           &flutter::StandardMethodCodec::GetInstance());
 
   channel_->SetMethodCallHandler(
@@ -38,7 +38,7 @@ void FileAssociationPlugin::RegisterWithRegistrar(
     flutter::PluginRegistrarWindows* registrar) {
   auto channel =
       std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "com.weimi95.weimi_file/file_association",
+          registrar->messenger(), "com.weimi95.weimi/file_association",
           &flutter::StandardMethodCodec::GetInstance());
 
   channel->SetMethodCallHandler(
